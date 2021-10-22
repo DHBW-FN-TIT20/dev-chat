2f89059a8b92b583b259d37379439cd91de9ee61
Johannes Brandenburger <johannes0709@icloud.com>
2021-10-22 10:45:29 +0200

    Update randomThreeName.py

--- randomThreeName.py
+++ randomThreeName.py
@@ -1,23 +1,17 @@
 import json
 from random import randint
 
-
-
 def get_three_random_nouns():
     with open('nouns.json', 'r') as file:
         obj = json.loads(file.read())
-    print(obj)
     noun_array = []
     for element in obj:
         noun_array.append(element["value"])
-    print(noun_array)
-
     threeWords = ""
-
     for index in [0, 1, 2]:
         random_index = randint(0, len(noun_array))
         current_word = noun_array[random_index]
         current_word = current_word.capitalize()
         threeWords = threeWords + current_word
-
     return threeWords
+
