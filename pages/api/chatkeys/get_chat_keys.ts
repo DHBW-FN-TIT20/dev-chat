54ccabe2e842370d4f5bfbca2c4230cb9efc3fc1
Johannes Brandenburger <johannes0709@icloud.com>
2022-01-26 11:56:52 +0100

    added api folders

---
+++ pages/api/chatkeys/get_chat_keys.ts
@@ -0,0 +1,20 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { IChatKey, IChatMessage } from '../../../public/interfaces';
+import { SupabaseConnenction } from '../supabaseAPI';
+
+type Data = {
+  placeholder: any
+}
+
+const supabaseConnenction = new SupabaseConnenction();
+
+/**
+ * !!! not working yet !!!
+ * This is a api route get chat keys.
+ * @param req the request object 
+ * @param res the response object 
+ */
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  res.status(200).json({ placeholder: "messages" });
+}
