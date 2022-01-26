548d985a96b0562f745072b9f63511249cedcc42
Johannes Brandenburger <johannes0709@icloud.com>
2022-01-26 12:01:01 +0100

    fixed imports

--- pages/api/chatkeys/get_chat_keys.ts
+++ pages/api/chatkeys/get_chat_keys.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { IChatKey, IChatMessage } from '../../../public/interfaces';
+import { IChatKey } from '../../../public/interfaces';
 import { SupabaseConnenction } from '../supabaseAPI';
 
 type Data = {
