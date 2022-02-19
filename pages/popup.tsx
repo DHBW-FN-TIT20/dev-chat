416c323b5e06cc7570d1570693e39c4c7d9ab1cc
withernext <janbr@gmx.de>
2022-02-19 19:21:26 +0100

    Worked on Popup
    first version, potential bug at less that 1006px width

--- pages/popup.tsx
+++ pages/popup.tsx
@@ -1,31 +1,63 @@
+import withRouter, { WithRouterProps } from 'next/dist/client/with-router'
 import React, { Component } from 'react'
-import styles from '../styles/Popup.module.css'
+import styles from '../styles/Popup.module.css' 
+import Image from 'next/image'
 
-export interface PopupState {
+export interface HeaderState {
 }
 
-export interface PopupProps {
-  headerText: string,
-  textDisplay: string,
-  closePopup: any,
+export interface HeaderProps extends WithRouterProps {
 }
 
-export default class Popup extends Component<PopupProps, PopupState> {
-  constructor(props: PopupProps) {
+class Header extends Component<HeaderProps, HeaderState> {
+  constructor(props: HeaderProps) {
     super(props)
     this.state = {
     }
+    
   }
+
+  componentDidMount() {}
+// noch ne variable für den header name
+  
+  /**
+   * Generates the JSX Output for the Client
+   * @returns JSX Output
+   */
   render() {
-    console.log(this.props.closePopup);
+    /**
+     * Initialize Router to navigate to other pages
+     */
+    const { router } = this.props
+
+  
     return (
-      <div className={styles.popup}>
-        <div className={styles.popup_inner}>
-          <h1>{this.props.headerText}</h1>
-          <p>{this.props.textDisplay}</p>
-          <button className={styles.buttonPopup} onClick={this.props.closePopup}>Close</button>
-        </div>
+      <div>  
+        <main>
+          <div className={styles.box}>
+	        <a className={styles.button} href="#popup1">Let me Pop up</a>
+          </div>
+
+          <div id="popup1" className={styles.overlay}>
+	          <div className={styles.popup}>
+              <div className={styles.delacc}>
+		          <p className={styles.delacchead}>Delete Account</p>
+              </div>
+              <div className={styles.attentiondiv}>
+              <span className={styles.attention}>!</span>
+              </div>
+		          <div className={styles.content}>
+		          	The User "Henry" was succesfully deleted.<br />You will be returned to the login page.
+		          </div>
+              <div className={styles.button1}>
+              <button className='return'>Ok</button>
+              </div>
+	        </div>
+          </div>
+        </main>
       </div>
-      );
+    )
   }
 }
+
+export default withRouter(Header)
