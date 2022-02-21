20207e35ff43c5741e41956872f565948141e11f
Johannes Brandenburger <johannes0709@icloud.com>
2022-02-21 11:46:00 +0100

    added get date by german string function

---
+++ shared/get_date_by_german_string.ts
@@ -0,0 +1,23 @@
+/**
+ * This function is used to create a date object from a string in german format.
+ * @param {string} germanString The string which should be converted to a date. Pattern: "DD.MM.YYYY-HH:MM".
+ * @returns The date object.
+ */
+export default function getDateByGermanString(germanString: string): Date | null {
+
+    if (germanString === undefined || germanString === "" || germanString === null || germanString.length !== 16) {
+        return null;
+    }
+
+    const day: number = parseInt(germanString.substring(0, 2));
+    const month: number = parseInt(germanString.substring(3, 5));
+    const year: number = parseInt(germanString.substring(6, 10));
+    const hour: number = parseInt(germanString.substring(11, 13));
+    const minute: number = parseInt(germanString.substring(14, 16));
+
+    if (isNaN(day) || isNaN(month) || isNaN(year) || isNaN(hour) || isNaN(minute)) {
+        return null;
+    }
+
+    return new Date(Date.UTC(year, month-1, day, hour, minute));
+}
