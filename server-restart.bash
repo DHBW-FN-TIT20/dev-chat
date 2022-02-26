4513f1c2c4fec8b0bfdeed1a8ee16c5aefaba55f
johannesbrandenburger <johannes0709@icloud.com>
2022-02-26 15:53:07 +0100

    JB: Created server restart bashscript

---
+++ server-restart.bash
@@ -0,0 +1,6 @@
+cd /home/jb/microSD/repos/dev-chat;
+GITTOKEN01=$(cat /home/jb/gitTokens/token01.txt);
+git pull https://johannesbrandenburger:$GITTOKEN01@github.com/DHBW-FN-TIT20/dev-chat;
+pm2 delete dev-chat;
+npm run build;
+pm2 start "npm run start" --name "dev-chat";
