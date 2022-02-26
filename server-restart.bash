14279b74a3b77bc7febe2f6ac72cb8a5d87b9cbc
johannesbrandenburger <johannes0709@icloud.com>
2022-02-26 16:01:09 +0100

    JB: Added #!/bin/bash to bash script

--- server-restart.bash
+++ server-restart.bash
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 cd /home/jb/microSD/repos/dev-chat;
 GITTOKEN01=$(cat /home/jb/gitTokens/token01.txt);
 git pull https://johannesbrandenburger:$GITTOKEN01@github.com/DHBW-FN-TIT20/dev-chat;
