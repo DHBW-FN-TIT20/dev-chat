29128d715b4787722db742fa23701fade4063ceb
Henry Schuler <72646334+schuler-henry@users.noreply.github.com>
2022-02-27 18:31:08 +0100

    Checked files in shared folder for codeguidelines.

--- shared/set_string_on_fix_length.ts
+++ shared/set_string_on_fix_length.ts
@@ -2,18 +2,16 @@
  * This function sets a string on a given length.
  * If the string is bigger than the fixLength it is cut.
  * If not, the missing chars are filled with spaces. 
- * @param {string} input The string which should be cut.
- * @param {number} fixLength The length of the result. 
- * @returns 
+ * @returns input with fix length
  */
 export function setStringOnFixLength(input: string, fixLength: number): string {
-    if (input.length > fixLength) {
-        return input.substring(0,fixLength)
-    } else {
-        let spacesToAdd = fixLength-input.length;
-        for (let i = 0; i < spacesToAdd; i++) {
-            input += " "
-        }
-        return input
+  if (input.length > fixLength) {
+    return input.substring(0, fixLength)
+  } else {
+    let spacesToAdd = fixLength - input.length;
+    for (let i = 0; i < spacesToAdd; i++) {
+      input += " "
     }
+    return input
+  }
 }
